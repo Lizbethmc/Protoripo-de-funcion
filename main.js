d627bbb1e10216896bef43f185aa0832c9729c19
Lizbethmc <98134851+Lizbethmc@users.noreply.github.com>
2022-03-30 23:29:58 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var x = 2;
+var y = 3;
+
+
+function suma(){
+ resultado = x + y;
+ console.log(`El resultado es ${resultado}`);
+}
+
+suma();
+
+
+
+
+//Se utiliza el parseInt para que lo interprete como texto, de lo contrario nos regresa un texto
+var n1 = parseInt(prompt("introduce un numero"));
+var n2 = parseInt(prompt("introduce otro numero"));
+
+var r = n1+n2;
+
+alert(r);
